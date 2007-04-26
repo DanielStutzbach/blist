c8d47ef06d9237685d8428f8852f90d2bd5dca6c
agthorr <agthorr@barsoom.org>
2007-04-26 14:22:19 +0000

    Prepared for release as an egg
    darcs-hash:20070426142219-a5408-99ada83f5ad65554dafc7df6aa6656b976f8a30f.gz

---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env
+
+from setuptools import setup, Extension
+
+setup(name='blist',
+      version='0.9.2',
+      description='a list-like type with better asymptotic performance',
+      author='Stutzbach Enterprises, LLC',
+      author_email='daniel@stutzbachenterprises.com',
+      url='http://stutzbachenterprises.com/blist/',
+      license = "BSD",
+      keywords = "blist list b+tree btree fast copy-on-write sparse",
+      ext_modules=[Extension('blist', ['blist.c'])],
+      requires = { 'python': '2.5' },
+      provides = ['blist'],
+      test_suite = "test_blist.test_suite",
+      classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Scientific/Engineering',
+            'License :: OSI Approved :: BSD License',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: C',
+            ],
+
+      long_description="""
+BList: a list-like type with better asymptotic performance
+=====
+
+The BList is a type that looks, acts, and quacks like a Python list(),
+but has better performance for many (but not all) use cases.  Below
+are some of the unique features of the BList:
+
+- just as fast as a Python list() when the list is small
+- getslice runs in O(log n) time
+- making a shallow copy runs in O(1) time
+- setslice runs in O(log n + log k) time if the inserted slice is a
+BList of length k
+- multipling a BList by k takes O(log k) time and O(log k) memory
+
+Example:
+
+>>> from blist import *
+>>> x = blist([0])
+>>> x *= 2**29
+>>> x.append(5)
+>>> y = x[4:-234234]
+>>> del x[3:1024]
+
+None of the above operations have a noticeable delay, even though the
+lists have over 500 million elements due to line 3.  The BList has two
+key features that allow it to pull this off this performance:
+
+1) Internally, a B+Tree is a wide, squat tree.  Each node has a
+maximum of 128 children.  If the entire list contains 128 or fewer
+objects, then there is only one node, which simply contains an array
+of the objects.  In other words, for short lists, a BList works just
+like Python's array-based list() type.  Thus, it has the same good
+performance on small lists.
+
+2) The BList type features transparent copy-on-write.  If a non-root
+node needs to be copied (as part of a getslice, copy, setslice, etc.),
+the node is shared between multiple parents instead of being copied.
+If it needs to be modified later, it will be copied at that time.
+This is completely behind-the-scenes; from the user's point of view,
+the BList works just like a regular Python list.
+
+So you can see the performance of the BList in more detail, several
+performance graphs available at the following link:
+    http://stutzbachenterprises.com/blist/
+""",
+            
+      )
--- test_blist.py
+++ test_blist.py
@@ -1,66 +1,75 @@
 #!/usr/bin/python
 
 """
-Copyright (C) 2007 Stutzbach Enterprises, LLC.
+Copyright 2007 Stutzbach Enterprises, LLC (daniel@stutzbachenterprises.com)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer. 
+   2. Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution. 
+   3. The name of the author may not be used to endorse or promote
+      products derived from this software without specific prior written
+      permission. 
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
 """
 
-
-"""
-Current known failures:
-- .sort() is unimplemented
-"""
 
 import sys
 import os
 
 import unittest
-from blist import BList
+import blist
 #BList = list
 from test import test_support, list_tests
 
-limit = 8
-n = 512
+limit = blist._limit
+n = 512/8 * limit
+blist = blist.blist
 
 class BListTest(list_tests.CommonTest):
-    type2test = BList
+    type2test = blist
 
     def test_truth(self):
         super(BListTest, self).test_truth()
-        self.assert_(not BList())
-        self.assert_(BList([42]))
+        self.assert_(not self.type2test())
+        self.assert_(self.type2test([42]))
 
     def test_identity(self):
-        self.assert_(BList([]) is not BList([]))
+        self.assert_(self.type2test([]) is not self.type2test([]))
 
     def test_len(self):
         super(BListTest, self).test_len()
-        self.assertEqual(len(BList()), 0)
-        self.assertEqual(len(BList([0])), 1)
-        self.assertEqual(len(BList([0, 1, 2])), 3)
+        self.assertEqual(len(self.type2test()), 0)
+        self.assertEqual(len(self.type2test([0])), 1)
+        self.assertEqual(len(self.type2test([0, 1, 2])), 3)
 
     def test_append(self):
-        lst = BList()
+        lst = self.type2test()
         t = tuple(range(n))
         for i in range(n):
             lst.append(i)
             self.assertEqual(tuple(lst), t[:i+1])
 
     def test_delstuff(self):
-        lst = BList(range(n))
+        lst = self.type2test(range(n))
         t = tuple(range(n))
         x = lst[4:258]
         self.assertEqual(tuple(x), tuple(t[4:258]))
@@ -74,14 +83,14 @@ class BListTest(list_tests.CommonTest):
         self.assertEqual(tuple(lst), tuple(t[0:200] + t[201:]))
 
     def test_del1(self):
-        lst2 = BList(range(limit+1))
+        lst2 = self.type2test(range(limit+1))
         self.assertEqual(tuple(lst2), tuple(range(limit+1)))
         del lst2[1]
         del lst2[-1]
         self.assertEqual(tuple(lst2), (0,) + tuple(range(2,limit)))
         
     def test_insert_and_del(self):
-        lst = BList(range(n))
+        lst = self.type2test(range(n))
         t = tuple(range(n))
         lst.insert(200, 0)
         self.assertEqual(tuple(lst), (t[0:200] + (0,) + t[200:]))
@@ -89,37 +98,37 @@ class BListTest(list_tests.CommonTest):
         self.assertEqual(tuple(lst), tuple(range(200)))
 
     def test_mul3(self):
-        lst = BList(range(3))
+        lst = self.type2test(range(3))
         self.assertEqual(tuple(lst*3), tuple(range(3)*3))
 
     def test_extendspam(self):
-        a = BList('spam')
+        a = self.type2test('spam')
         a.extend('eggs')
         self.assertEqual(list(a), list('spameggs'))
 
     def test_bigmul1(self):
-        x = BList([0])
+        x = self.type2test([0])
         for i in range(290) + [1000, 10000, 100000, 1000000, 10000000, 2**29]:
             self.assertEqual(len(x*i), i)
 
     def test_badinit(self):
-        self.assertRaises(TypeError, BList, 0, 0, 0)
+        self.assertRaises(TypeError, self.type2test, 0, 0, 0)
 
     def test_copyself(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         x[:] = x
 
     def test_nohash(self):
-        x = BList()
+        x = self.type2test()
         d = {}
         self.assertRaises(TypeError, d.__setitem__, x, 5)
 
     def test_collapseboth(self):
-        x = BList(range(512))
+        x = self.type2test(range(512))
         del x[193:318]
 
     def test_collapseright(self):
-        x = BList(range(512))
+        x = self.type2test(range(512))
         del x[248:318]
 
     def test_badrepr(self):
@@ -130,27 +139,27 @@ class BListTest(list_tests.CommonTest):
             def __repr__(self):
                 raise BadExc
 
-        x = BList([BadRepr()])
+        x = self.type2test([BadRepr()])
         self.assertRaises(BadExc, repr, x)
-        x = BList(range(n))
+        x = self.type2test(range(n))
         x.append(BadRepr())
         self.assertRaises(BadExc, repr, x)
 
     def test_slice0(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         x[slice(5,3,1)] = []
         self.assertEqual(x, list(range(n)))
-        x = BList(range(n))
+        x = self.type2test(range(n))
         self.assertRaises(ValueError, x.__setitem__, slice(5,3,1), [5,3,2])
         del x[slice(5,3,1)]
         self.assertEqual(x, list(range(n)))
 
     def test_badindex(self):
-        x = BList()
+        x = self.type2test()
         self.assertRaises(TypeError, x.__setitem__, 's', 5)
 
     def test_comparelist(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         y = list(range(n-1))
         self.assert_(not (x == y))
         self.assert_(x != y)
@@ -168,8 +177,8 @@ class BListTest(list_tests.CommonTest):
         self.assert_(x != y)
 
     def test_compareblist(self):
-        x = BList(range(n))
-        y = BList(range(n-1))
+        x = self.type2test(range(n))
+        y = self.type2test(range(n-1))
         self.assert_(not (x == y))
         self.assert_(x != y)
         self.assert_(not (x < y))
@@ -182,34 +191,39 @@ class BListTest(list_tests.CommonTest):
         self.assert_(x != y)
 
     def test_comparetuple(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         y = tuple(range(n))
         self.assert_(x != y)
 
     def test_indexempty(self):
-        x = BList(range(10))
+        x = self.type2test(range(10))
         self.assertRaises(ValueError, x.index, 'spam')
 
     def test_indexargs(self):
-        x = BList(range(10))
+        x = self.type2test(range(10))
         self.assertEqual(x.index(5,1,-1), 5)
         self.assertRaises(ValueError, x.index, 5, -1, -9)
         self.assertRaises(ValueError, x.index, 8, 1, 4)
         self.assertRaises(ValueError, x.index, 0, 1, 4)
 
     def test_reversebig(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         x.reverse()
         self.assertEqual(x, range(n-1,-1,-1))
 
     def test_badconcat(self):
-        x = BList()
+        x = self.type2test()
         y = 'foo'
         self.assertRaises(TypeError, x.__add__, y)
 
     def test_bad_assign(self):
-        x = BList(range(n))
+        x = self.type2test(range(n))
         self.assertRaises(TypeError, x.__setitem__, slice(1,10,2), 5)
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(BListTest))
+    return suite                                            
 
 def test_main(verbose=None):
     test_support.run_unittest(BListTest)
