155750db7cff80924b37d7f53e5706dd3f728764
Daniel Stutzbach <daniel@stutzbachenterprises.com>
2009-03-31 16:55:40 +0000

    Added more tests and bumped version
    darcs-hash:20090331165540-7e00d-5b8e0991de06f3aab66a83dd191851115958399b.gz

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
     from distutils.core import setup, Extension
 
 setup(name='blist',
-      version='0.9.15',
+      version='0.9.16',
       description='a list-like type with better asymptotic performance and similar performance on small lists',
       author='Stutzbach Enterprises, LLC',
       author_email='daniel@stutzbachenterprises.com',
