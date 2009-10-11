e7c0534524918aa964b2bad45bf02b8e4f3aaf2a
Michel Alexandre Salim <salimma@fedoraproject.org>
2009-10-10 22:59:53 -0400

    gcc: use fastcall on ix86 only

--- listobject.h
+++ listobject.h
@@ -37,7 +37,7 @@ extern "C" {
 /* fastest possible local call under MSVC */
 #define BLIST_LOCAL(type) static type __fastcall
 #define BLIST_LOCAL_INLINE(type) static __inline type __fastcall
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) && defined(__i386__)
 #define BLIST_LOCAL(type) static type __attribute__((fastcall))
 #define BLIST_LOCAL_INLINE(type) static inline __attribute__((fastcall)) type
 #else
