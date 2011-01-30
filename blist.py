163a61f5d3b8c8cc6be3d6200a29b6c383d86086
Daniel Stutzbach <daniel@stutzbachenterprises.com>
2011-01-30 15:32:31 -0800

    Issue 25: The blist type should register as a MutableSequence

--- blist.py
+++ blist.py
@@ -4,4 +4,5 @@ if hasattr(collections, 'MutableSet'): # Only supported in Python 2.6+
     from _sortedlist import sortedlist, sortedset, weaksortedlist, weaksortedset
     from _sorteddict import sorteddict
     from _btuple import btuple
+    collections.MutableSequence.register(blist)
 del collections
