e67b1e8d7ec2eddd32ef3aed81e03701e69e8cec
Daniel Stutzbach <daniel@stutzbachenterprises.com>
2011-03-19 11:11:06 -0700

    Mark as supporting Python 3.2

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='blist',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
             ],
       long_description=open('README.rst').read()
 )
